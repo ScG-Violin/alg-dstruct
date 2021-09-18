6cb78eeba15628c754bbdbeef847f0207953f5e5
ScG-Violin <tolya1409@mail.ru>
2021-09-18 11:34:28 +0300

    Main.c + Basic test

--- 1-Kulyanin-A18/src/main.c
+++ 1-Kulyanin-A18/src/main.c
@@ -1,3 +1,16 @@
+#include "str_sor.h"
+#include <stdlib.h>
+#include <stdio.h>
+
 int main(void) {
+	char* more = "more", *less = "less", *same = "same", *empty= "";
+	list_* list = ListCreate();
+	AddCell(CellCreate(more), list);
+	AddCell(CellCreate(less), list);
+	AddCell(CellCreate(same), list);
+	AddCell(CellCreate(same), list);
+	AddCell(CellCreate(empty), list);
+	ListPrint(list);
+	ListDestroy(list);
 	return 0;
 }
