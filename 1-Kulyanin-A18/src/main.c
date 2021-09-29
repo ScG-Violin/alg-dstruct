415e91d5eaaea49fde900d7d82a7ce92cf4a5ece
ScG-Violin <tolya1409@mail.ru>
2021-09-29 21:29:44 +0300

    Reworked main for connect with Google Test

--- 1-Kulyanin-A18/src/main.c
+++ 1-Kulyanin-A18/src/main.c
@@ -3,22 +3,6 @@
 #include <stdio.h>
 
 int main(void) {
-	char* more = "more", *less = "less", *same = "same", *empty= "";
-	list_* list = ListCreate(), *listUn = ListCreate();
-	AddCellSort(CellCreate(more), list);
-	AddCellSort(CellCreate(less), list);
-	AddCellSort(CellCreate(same), list);
-	AddCellSort(CellCreate(same), list);
-	AddCellSort(CellCreate(empty), list);
-	ListPrint(list);
-	ListDestroy(list);
-	AddCell(CellCreate(same), listUn, 0);
-	AddCell(CellCreate(more), listUn, 1);
-	AddCell(CellCreate(same), listUn, 43);
-	AddCell(CellCreate(less), listUn, 2);
-	AddCell(CellCreate(empty), listUn,2);
-	ListBublSort(listUn);
-	ListPrint(listUn);
-	ListDestroy(listUn);
-	return 0;
+	printf("Wake the f*ck Up SAMURAI, we have a C to burn."); 
+	return RUN_ALL_TESTS();
 }
